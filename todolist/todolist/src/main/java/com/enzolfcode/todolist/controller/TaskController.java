dd3f81e6656cd6356e0936a9debd4c42ebf83295
Enzo L. <fenzolemos@gmail.com>
2024-09-09 14:42:23 -0300

    feat: endpoint POST para criarTask no TaskController

--- todolist/todolist/src/main/java/com/enzolfcode/todolist/controller/TaskController.java
+++ todolist/todolist/src/main/java/com/enzolfcode/todolist/controller/TaskController.java
@@ -1,4 +1,22 @@
 package com.enzolfcode.todolist.controller;
 
+
+import com.enzolfcode.todolist.model.Task;
+import com.enzolfcode.todolist.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TaskController {
+
+    @Autowired
+    TaskService taskService;
+
+    @PostMapping("/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task criarTask (@RequestBody Task task){
+        return taskService.criarTask(task);
+    }
 }
