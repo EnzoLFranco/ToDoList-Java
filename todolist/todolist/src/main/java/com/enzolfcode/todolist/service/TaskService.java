bbb87173db37551da7cb2be24e47e9381e789a9f
Enzo L. <fenzolemos@gmail.com>
2024-09-09 14:19:54 -0300

    feat: criarTask no TaskService

--- todolist/todolist/src/main/java/com/enzolfcode/todolist/service/TaskService.java
+++ todolist/todolist/src/main/java/com/enzolfcode/todolist/service/TaskService.java
@@ -1,4 +1,19 @@
 package com.enzolfcode.todolist.service;
 
+import com.enzolfcode.todolist.model.Task;
+import com.enzolfcode.todolist.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task criarTask (Task task){
+        return taskRepository.save(task);
+    }
 }
