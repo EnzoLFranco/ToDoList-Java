d3c1d86eb4f5be5a701663480e0f404fabd4bbc6
Enzo L. <fenzolemos@gmail.com>
2024-09-09 14:55:05 -0300

    feat: endpoint GET para ListarTodasAsTask no TaskController

--- todolist/todolist/src/main/java/com/enzolfcode/todolist/controller/TaskController.java
+++ todolist/todolist/src/main/java/com/enzolfcode/todolist/controller/TaskController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
 public class TaskController {
@@ -19,4 +21,13 @@ public class TaskController {
     public Task criarTask (@RequestBody Task task){
         return taskService.criarTask(task);
     }
+
+    @GetMapping("/tasks")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Task> getTodasAsTasks(){
+        return taskService.listarTodasAsTasks();
+    }
+
+    
+
 }
