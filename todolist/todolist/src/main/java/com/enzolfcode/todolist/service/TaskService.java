db1fbc616a8d006e8ea0f1dbc6f4a9ec57f52eb0
Enzo L. <fenzolemos@gmail.com>
2024-09-11 16:36:17 -0300

    feat: atualizaTaskPorID no TaskService

--- todolist/todolist/src/main/java/com/enzolfcode/todolist/service/TaskService.java
+++ todolist/todolist/src/main/java/com/enzolfcode/todolist/service/TaskService.java
@@ -29,4 +29,15 @@ public class TaskService {
                 .map(task -> ResponseEntity.ok().body(task))
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    public ResponseEntity<Task> atualizaTaskPorID(Task task, Long id){
+        return taskRepository.findById(id)
+                .map(taskToUpdate ->{
+                    taskToUpdate.setTitulo(task.getTitulo());
+                    taskToUpdate.setDescricao(task.getDescricao());
+                    taskToUpdate.setPrazoFinal(task.getPrazoFinal());
+                    Task atualizado = taskRepository.save(taskToUpdate);
+                    return ResponseEntity.ok().body(atualizado);
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
